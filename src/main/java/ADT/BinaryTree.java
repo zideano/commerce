b28a6e0a08e99f8c2bfa810d7c3c271a53bbcbdc
zideano <jermaineandrethomas@gmail.com>
2018-03-17 21:47:26 +0000

    Binary tree data structure with test cases.

--- src/main/java/ADT/BinaryTree.java
+++ src/main/java/ADT/BinaryTree.java
@@ -1,11 +1,271 @@
 package ADT;
 
-public class BinaryTree<T> {
-    private Node<T> left;
-    private Node<T> right;
-    private T data;
+import java.util.Objects;
 
-    public BinaryTree(T data) {
-        this.data = data;
+/**
+ * Generic type binary tree root structure, which is composed
+ * of a single root node, and where each nodes have no more
+ * than 2 child nodes and a final node called a leaf node when
+ * there is no further child nodes (sub-nodes).
+ *
+ * Order: left node is always less than the right nodes
+ *
+ * @param <T>
+ */
+public class BinaryTree<T> {
+    private BinaryTree<T> left;
+    private BinaryTree<T> right;
+    private T root;
+
+    public BinaryTree(T root) {
+        this.root = root;
+    }
+
+    /**
+     * Add new root to our binary tree
+     *
+     * @param data
+     */
+    public void insert(T data) {
+        if (data instanceof Integer) {
+            isInteger((Integer) data);
+        } else if (data instanceof String) {
+            isString((String) data);
+        } else if (data instanceof Character) {
+            isCharacter((Character) data);
+        } else if (data instanceof Comparable) {
+            isObject((Comparable) data);
+        } else {
+            throw new RuntimeException("Unsupported root type -> " + data.getClass().getName());
+        }
+    }
+
+    /**
+     * This method checks for the respective root type
+     * then search the binary tree for the root.
+     *
+     * @param data to be searched for in the binary tree
+     * @return true if root is found otherwise return false;
+     */
+    public boolean find(T data) {
+        if (data instanceof Integer) {
+            return findInteger((Integer) data);
+        } else if (data instanceof String) {
+           return findString((String) data);
+        } else if (data instanceof Character) {
+            return findCharacter((Character) data);
+        } else if (data instanceof Comparable) {
+           return findComparable((Comparable) data);
+        } else {
+            throw new RuntimeException("Unsupported root type -> " + data.getClass().getName());
+        }
+    }
+
+    private void isInteger(Integer val) {
+        if (val < (Integer) this.root) {
+            if (left == null) {
+                left = new BinaryTree<>((T) val);
+            } else {
+                left.isInteger(val);
+            }
+        } else if (val == this.root) {
+            System.out.println("The integer -> " + val + " is already in the tree ");
+        } else {
+            if (right == null) {
+                right = new BinaryTree<>((T) val);
+            } else {
+                right.isInteger(val);
+            }
+        }
+    }
+
+    private void isString(String val) {
+        int res = val.compareTo((String) this.root);
+
+        if (res < 0) {
+            if (left == null) {
+                left = new BinaryTree<>((T) val);
+            } else {
+                left.isString(val);
+            }
+        } else if (res == 0){
+            System.out.println("The string -> " + val + " is already in the tree ");
+            return;
+        } else {
+            if (right == null) {
+                right = new BinaryTree<>((T) val);
+            } else {
+                right.isString(val);
+            }
+        }
+    }
+
+    private void isCharacter(Character val) {
+        int res = val.compareTo((Character) this.root);
+
+        if (res < 0) {
+            if (left == null) {
+                left = new BinaryTree<>((T) val);
+            } else {
+                left.isCharacter(val);
+            }
+        } else if (res == 0) {
+            System.out.println("The character -> " + val + " is already in the tree ");
+            return;
+        } else {
+            if (right == null) {
+                right = new BinaryTree<>((T) val);
+            } else {
+                right.isCharacter(val);
+            }
+        }
+    }
+
+    private void isObject(Comparable val) {
+        int res = val.compareTo((Comparable) this.root);
+
+        if (res < 0) {
+            if (left == null) {
+                left = new BinaryTree<>((T) val);
+            } else {
+                left.isObject(val);
+            }
+        } else if (res == 0) {
+            System.out.println("The comparable object -> " + val + " is already in the tree ");
+            return;
+        } else {
+            if (right == null) {
+                right = new BinaryTree<>((T) val);
+            } else {
+                right.isObject(val);
+            }
+        }
+    }
+
+    private boolean findInteger(Integer val) {
+        if (val == this.root) {
+            return true;
+        } else if (val < (Integer) this.root) {
+            if (left == null) {
+                return false;
+            } else {
+                return left.findInteger(val);
+            }
+        } else {
+            if (right == null) {
+                return false;
+            } else {
+                return right.findInteger(val);
+            }
+        }
+    }
+
+    private boolean findString(String val) {
+        int res = val.compareTo((String) this.root);
+
+        if (res == 0) {
+            return true;
+        } else if (res < 0){
+            if (left == null) {
+                return false;
+            } else {
+                return left.findString(val);
+            }
+        } else {
+            if (right == null) {
+                return false;
+            } else {
+                return right.findString(val);
+            }
+        }
+    }
+
+    private boolean findCharacter(Character val) {
+        int res = val.compareTo((Character) this.root);
+
+        if (res == 0 ) {
+          return true;
+        } else if (res < 0) {
+            if (left == null) {
+                return false;
+            } else {
+                return left.findCharacter(val);
+            }
+        } else {
+            if (right == null) {
+                return false;
+            } else {
+               return right.findCharacter(val);
+            }
+        }
+    }
+
+    private boolean findComparable(Comparable val) {
+        int res = val.compareTo(this.root);
+
+        if (res == 0) {
+            return true;
+        } else if (res < 0) {
+            if (left == null) {
+                return false;
+            } else {
+                return left.findComparable(val);
+            }
+        } else {
+            if (right == null) {
+                return false;
+            } else {
+                return right.findComparable(val);
+            }
+        }
+    }
+
+    /**
+     * Traverse the binary tree using the inorder traversal
+     * procedure, traveling from the left node, root node to
+     * the right node.
+     *
+     *              (root)
+     *              /    \
+     *             /      \
+     *            /        \
+     *           /          \
+     *         (left)       (right)
+     */
+    public void traverse() {
+        if (left != null) {
+            left.traverse();
+        }
+
+        System.out.println(root);
+
+        if (right != null) {
+            right.traverse();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BinaryTree<?> that = (BinaryTree<?>) o;
+        return Objects.equals(left, that.left) &&
+                Objects.equals(right, that.right) &&
+                Objects.equals(root, that.root);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(left, right, root);
+    }
+
+    @Override
+    public String toString() {
+        return "BinaryTree{" +
+                "left=" + left +
+                ", right=" + right +
+                ", root=" + root +
+                '}';
     }
 }
---
+++ src/test/java/BinaryTreeTest.java
@@ -0,0 +1,107 @@
+import ADT.BinaryTree;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinaryTreeTest {
+
+    @Test
+    public void testBinaryTreeWithInteger() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>(6);
+        binaryTree.insert(2);
+        binaryTree.insert(7);
+        binaryTree.insert(8);
+        binaryTree.insert(4);
+
+        boolean eight = binaryTree.find(8);
+        boolean six = binaryTree.find(6);
+        boolean two = binaryTree.find(2);
+        boolean seven = binaryTree.find(7);
+        boolean four = binaryTree.find(4);
+
+        boolean wrong = binaryTree.find(10);
+
+        Assert.assertTrue(eight);
+        Assert.assertTrue(two);
+        Assert.assertTrue(seven);
+        Assert.assertTrue(six);
+        Assert.assertTrue(four);
+        Assert.assertFalse(wrong);
+    }
+
+    @Test
+    public void testBinaryTreeWithString() {
+        BinaryTree<String> binaryTree = new BinaryTree<>("tree");
+        binaryTree.insert("data");
+        binaryTree.insert("binary");
+        binaryTree.insert("amour");
+        binaryTree.insert("random");
+
+        boolean rand = binaryTree.find("random");
+        boolean amour = binaryTree.find("amour");
+        boolean binary = binaryTree.find("binary");
+        boolean data = binaryTree.find("data");
+        boolean tree = binaryTree.find("tree");
+
+        boolean wrong = binaryTree.find("wrong");
+
+        Assert.assertTrue(rand);
+        Assert.assertTrue(binary);
+        Assert.assertTrue(data);
+        Assert.assertTrue(amour);
+        Assert.assertTrue(tree);
+        Assert.assertFalse(wrong);
+    }
+
+    @Test
+    public void testBinaryTreWithCharacter() {
+        BinaryTree<Character> binaryTree = new BinaryTree<>('c');
+        binaryTree.insert('k');
+        binaryTree.insert('z');
+        binaryTree.insert('p');
+        binaryTree.insert('w');
+
+        boolean c = binaryTree.find('c');
+        boolean k = binaryTree.find('k');
+        boolean z = binaryTree.find('z');
+        boolean p = binaryTree.find('p');
+        boolean w = binaryTree.find('w');
+
+        boolean wrong = binaryTree.find('a');
+
+        Assert.assertTrue(c);
+        Assert.assertTrue(z);
+        Assert.assertTrue(p);
+        Assert.assertTrue(k);
+        Assert.assertTrue(w);
+        Assert.assertFalse(wrong);
+    }
+
+    @Test
+    public void testBinaryTreeTraversal() {
+        BinaryTree<Character> binaryTree = new BinaryTree<>('c');
+        binaryTree.insert('k');
+        binaryTree.insert('z');
+        binaryTree.insert('p');
+        binaryTree.insert('w');
+
+        BinaryTree<Character> binaryTree2 = new BinaryTree<>('c');
+        binaryTree2.insert('k');
+        binaryTree2.insert('z');
+        binaryTree2.insert('p');
+        binaryTree2.insert('w');
+
+        BinaryTree<Character> binaryTree3 = new BinaryTree<>('c');
+        binaryTree3.insert('k');
+        binaryTree3.insert('z');
+        binaryTree3.insert('q');
+        binaryTree3.insert('w');
+
+        String result = binaryTree.toString();
+        String result2 = binaryTree2.toString();
+        String result3 = binaryTree3.toString();
+
+        Assert.assertEquals(result, result2);
+        Assert.assertNotEquals(result, result3);
+        Assert.assertNotEquals(result2, result3);
+    }
+}
