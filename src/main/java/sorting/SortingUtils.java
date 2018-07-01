e95aacf208dec2275d81c9deade878f46583b62a
zideano <jermaineandrethomas@gmail.com>
2018-07-01 14:53:24 +0100

    Removed unused variable.

--- src/main/java/sorting/SortingUtils.java
+++ src/main/java/sorting/SortingUtils.java
@@ -194,7 +194,6 @@ public class SortingUtils {
      * @return the sorted array
      */
     public static int[] bubbleSort(int[] arr) {
-        boolean sorted = false;
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
